852521d46ca061bcac75c5d5ef341174adeff7fa
Salsa28 <80502308+Salsa28@users.noreply.github.com>
2022-04-06 23:50:43 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 
 <?php
-$conn_string = "host=satao.db.elephantsql.com port=5432 " . 
+$conn_string = "host=john.db.elephantsql.com port=5432 " . 
     "dbname=hrpawlxx user=hrpawlxx " .
     "password=7HPJO6zCvce5EnrqcP69eMhyJ1A-HK-B";
 $conn = pg_connect($conn_string);
