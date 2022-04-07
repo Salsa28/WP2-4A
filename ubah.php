d3ab7a01559e3999187d5a86cbd3ae1e1323b95c
Salsa28 <80502308+Salsa28@users.noreply.github.com>
2022-04-07 06:09:08 -0700

    Update ubah.php

--- ubah.php
+++ ubah.php
@@ -9,24 +9,24 @@ $data = pg_fetch_object($result);
 <h3>Form Ubah Data</h3>
 
 <form method="post">
-    NIM : 
-<?php echo '<input type="text" name="nim" value="' . $data->nim . '" readonly></br>' ?>
-    NAMA : 
-<?php echo '<input type="text" name="nama" value="' . $data->nama . '"></br>' ?>
-    KELAS : 
-<?php echo '<input type="text" name="kelas" value="' . $data->kelas . '"></br>' ?>
+    NIM :
+    <?php echo '<input type="text" name="nim" value="' . $data->nim . '" readonly><br>' ?>
+    NAMA :
+    <?php echo '<input type="text" name="nama" value="' . $data->nama . '"><br>' ?>
+    KELAS :
+    <?php echo '<input type="text" name="kelas" value="' . $data->kelas . '"><br>' ?>
     <input type="submit" value="Simpan">
 </form>
 
 <?php
-if(isset($_POST['nim']) and !empty($_POST['nim'])) {
-    $sql = "update mahasiswa set nama='" . $_POST['nama'] . "', kelas='" . $_POST['kelas'] . "' " .
-        "where nim='" . $_POST['nim'] . "'";
+if (isset($_POST['nim']) and !empty($_POST['nim'])) {
+    $sql = "update mahasiswa set nama='" . $_POST['nama'] . "', kelas='" . $_POST['kelas'] .
+        "' where nim='" . $_POST['nim'] . "'";
     $result = pg_affected_rows(pg_query($sql));
-    if($result == 1) {
+    if ($result == 1) {
         echo '<script type="text/javascript">';
-        echo 'alert("Perubahan telah tersimpan");';
-        echo 'window.location.href = "index.php";';
+        echo 'alert("Perubahan data telah tersimpan");';
+        echo 'window.location.href="index.php";';
         echo '</script>';
     }
 }
